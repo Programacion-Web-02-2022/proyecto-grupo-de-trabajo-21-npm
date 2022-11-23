12c01290233253e25dfcceedb74283eb61ef9ca7
Christian Lopez <christian844@LAPTOP-M9O9SHE5.localdomain>
2022-11-23 00:13:16 -0600

    :pencil: PetProfile, Dates and DatesHistory interfaces

--- Client/Paws/src/Pages/User/PetProfile.jsx
+++ Client/Paws/src/Pages/User/PetProfile.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import LoggedNavbar from "../../Components/User/LoggedNavBar/LoggedNavBar";
 import LoggedFooter from "../../Components/User/LoggedFooter/LoggedFooter";
+import PetProfileContainer from "../../Components/User/PetProfile/PetProfileContainer";
 
 const PetProfile = () =>{
     return(
         <div  className="bg-background">
             <LoggedNavbar/>
+            <PetProfileContainer/>
             <LoggedFooter/>
         </div>
 
