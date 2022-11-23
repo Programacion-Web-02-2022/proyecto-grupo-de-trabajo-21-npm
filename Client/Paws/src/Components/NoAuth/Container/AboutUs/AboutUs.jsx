8ce82a02cb70c24e390d316284706ef051499344
Elmer-sv <00155620@uca.edu.sv>
2022-11-23 13:43:16 -0600

    :memo: Added the Info component in About us file

--- Client/Paws/src/Components/NoAuth/Container/AboutUs/AboutUs.jsx
+++ Client/Paws/src/Components/NoAuth/Container/AboutUs/AboutUs.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Info from "./Info/Info";
 
 
 const AboutUs = () => {
     return (
         <div className="flex flex-col mx-20 justify-center h-screen w-2/6 ">
-
+            <Info />
         </div>
     );
 };
